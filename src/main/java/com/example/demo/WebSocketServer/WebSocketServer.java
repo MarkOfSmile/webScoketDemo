dffbe820a68523318130d32b776c541d8ad32bc1
yunkang <yunkang@yunkang.lan>
2019-06-24 22:15:17 +0800

    merge form ken

--- src/main/java/com/example/demo/WebSocketServer/WebSocketServer.java
+++ src/main/java/com/example/demo/WebSocketServer/WebSocketServer.java
@@ -1,63 +1,70 @@
 package com.example.demo.WebSocketServer;
- 
-import com.example.demo.getHttpSessionConfigurator.*;
-import org.springframework.stereotype.Component;
- 
-import javax.servlet.http.HttpSession;
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
+
 import java.io.IOException;
 import java.util.concurrent.CopyOnWriteArraySet;
- 
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.getHttpSessionConfigurator.GetHttpSessionConfigurator;
+
 /**
  * Created by jack on 2017/10/25.
  */
 /**
  * websocket的具体实现类
  */
-@ServerEndpoint(value = "/websocket",configurator = GetHttpSessionConfigurator.class)
+@ServerEndpoint(value = "/websocket", configurator = GetHttpSessionConfigurator.class)
 @Component
 public class WebSocketServer {
-    //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
+    // 静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
-    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    // concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
     private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
-    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    // 与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
- 
+
     /**
      * 连接建立成功调用的方法
      */
     @OnOpen
-    public void onOpen(Session session,EndpointConfig config) {
+    public void onOpen(Session session, EndpointConfig config) {
         this.session = session;
-        HttpSession httpSession= (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
-        System.out.println("name is : "+ httpSession.getAttribute("name"));
-        //sessionMap.put(session.getId(), session);
-        System.out.println("the session is : "+session.getId());
-        System.out.println("session content is : "+session.getId());
-        System.out.println("httpSession  is : "+httpSession.getId());
-        webSocketSet.add(this);     //加入set中
-        addOnlineCount();           //在线数加1
+        HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+        System.out.println("name is : " + httpSession.getAttribute("name"));
+        // sessionMap.put(session.getId(), session);
+        System.out.println("the session is : " + session.getId());
+        System.out.println("session content is : " + session.getId());
+        System.out.println("httpSession  is : " + httpSession.getId());
+        webSocketSet.add(this); // 加入set中
+        addOnlineCount(); // 在线数加1
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
         try {
-            //sendMessage(CommonConstant.CURRENT_WANGING_NUMBER.toString());
-            sendMessage("hello I am webScoket Client in demo 2");
+            // sendMessage(CommonConstant.CURRENT_WANGING_NUMBER.toString());
+            sendMessage("Hello I am WebSocketServer");
         } catch (IOException e) {
             System.out.println("IO异常");
         }
     }
- 
+
     /**
      * 连接关闭调用的方法
      */
     @OnClose
     public void onClose() {
-        webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
+        webSocketSet.remove(this); // 从set中删除
+        subOnlineCount(); // 在线数减1
         System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
     }
- 
+
     /**
      * 收到客户端消息后调用的方法
      *
@@ -66,8 +73,8 @@ public class WebSocketServer {
     @OnMessage
     public void onMessage(String message, Session session) {
         System.out.println("来自客户端的消息:" + message);
-        System.out.println("onMessage sessionId is : "+session.getId());
-        //群发消息
+        System.out.println("onMessage sessionId is : " + session.getId());
+        // 群发消息
         for (WebSocketServer item : webSocketSet) {
             try {
                 item.sendMessage(message);
@@ -76,7 +83,7 @@ public class WebSocketServer {
             }
         }
     }
- 
+
     /**
      * 发生错误时调用
      */
@@ -85,12 +92,12 @@ public class WebSocketServer {
         System.out.println("发生错误");
         error.printStackTrace();
     }
- 
+
     public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText(message+" : sendMessage id is : "+this.session.getId());
-        //this.session.getAsyncRemote().sendText(message);
+        this.session.getBasicRemote().sendText(message + " : sendMessage id is : " + this.session.getId());
+        // this.session.getAsyncRemote().sendText(message);
     }
- 
+
     /**
      * 群发自定义消息
      */
@@ -103,15 +110,15 @@ public class WebSocketServer {
             }
         }
     }
- 
+
     public static synchronized int getOnlineCount() {
         return onlineCount;
     }
- 
+
     public static synchronized void addOnlineCount() {
         WebSocketServer.onlineCount++;
     }
- 
+
     public static synchronized void subOnlineCount() {
         WebSocketServer.onlineCount--;
     }
